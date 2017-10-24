3b3d67cf51048a95e216b11af59e25828975b2d2
Eugeniy Meshcheryakov <eugen@debian.org>
2017-10-24 23:02:25 +0200

    Improve errors handling when reading Unicode blocks at runtime

--- read_blocks.c
+++ read_blocks.c
@@ -10,8 +10,10 @@ struct unicode_block *read_blocks(const char *file_name, int *n)
 	*n = 0;
 
 	FILE *input_file = fopen(file_name, "r");
-	if (!input_file)
-		abort(); // TODO
+	if (!input_file) {
+		perror("fopen");
+		exit(7);
+	}
 
 	char *line = NULL;
 	size_t len = 0;
@@ -29,10 +31,25 @@ struct unicode_block *read_blocks(const char *file_name, int *n)
 			struct unicode_block *b = blocks + *n;
 			b->start = block_start;
 			b->end = block_end;
-			b->name = strdup(block_name); // TODO check for zero
+			b->name = strdup(block_name);
+			if (b->name == NULL) {
+				perror("strdup");
+				exit(8);
+			}
+
 			*n += 1;
-			if (*n >= nalloc)
-				break; // TODO handle this better
+			if (*n >= nalloc) {
+				int new_nalloc = nalloc + 256;
+				struct unicode_block *new_blocks = realloc(blocks,
+					new_nalloc * sizeof(struct unicode_block));
+				if (new_blocks == NULL) {
+					perror("realloc");
+					exit(9);
+				}
+				memset(new_blocks + nalloc, 0, (new_nalloc - nalloc) * sizeof(struct unicode_block));
+				nalloc = new_nalloc;
+				blocks = new_blocks;
+			}
 		}
 	}
 	free(line);
